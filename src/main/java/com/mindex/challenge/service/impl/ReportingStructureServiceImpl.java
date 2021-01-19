0a497a234e899710c904e3b93045f461700857de
jakedodd <doddj@duq.edu>
2021-01-19 00:01:22 -0500

    Final Commit

--- src/main/java/com/mindex/challenge/service/impl/ReportingStructureServiceImpl.java
+++ src/main/java/com/mindex/challenge/service/impl/ReportingStructureServiceImpl.java
@@ -2,7 +2,12 @@ package com.mindex.challenge.service.impl;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -41,37 +46,31 @@ public class ReportingStructureServiceImpl implements ReportingStructureService 
 	/*
 	 * The implementation of this method depends on the interpretation of --> 
 	 * "The number of reports is determined to be the number of directReports for an employee and all of their distinct reports."
-	 * I have interpreted this to mean the employees direct reports, and all distinct reports one layer  down the reporting structure. 
-	 * The implementation would change if we are also counting reports further down
+	 * I interpret this to mean all distinct reports under the given employee
 	 */
-    private int getNumberOfReports(Employee employee) { 
-    	List<Employee> distinctReports = employee.getDirectReports(); 
-    	List<Employee> subReports = new ArrayList<Employee>(); 
+    private int getNumberOfReports(Employee employee) {
+    	List<Employee> distinctReports = new ArrayList<Employee>();
+    	Queue<Employee> allReports = new LinkedList<Employee>();
     	
-    	// First we need to check if the list of directReports for this employee is null. If it is we can just return 0. Without doing this check first, null lists will cause problems.
-    	if(distinctReports == null || distinctReports.size() == 0) {
-    		return 0;
-    	} else {
-	    	int numberOfReports = distinctReports.size();
-		    // For each of the directReports, we get the full Employee object, and add all of their reports to the list of subReports.
-		    for(Employee e: distinctReports) { 
-		    	Employee directReport = employeeRepository.findByEmployeeId(e.getEmployeeId());
-		        if(directReport.getDirectReports() != null) {
-		        	subReports.addAll(directReport.getDirectReports());
-		        } 
-		    } 
-		    // For each Employee in the subReports list, we check to make sure it doesn't exist in our list of distinct reports. If it does not, we increase our total
-		    // numberOfReports counter by one, and add that employee to the distinct list of employees for future checks
-		    for(int i = 0; i < subReports.size(); i++) { 
-		    	if(!distinctReports.contains(subReports.get(i))) {
-		    		numberOfReports++;
-		    		distinctReports.add(subReports.get(i));
-		    	} 
-		    } 
-		    return numberOfReports;
+    	allReports.add(employee);
+    	
+    	while(!allReports.isEmpty()) {
+    		Employee currentEmployee = allReports.remove();
+    		// The array list returned by getDirectReports contains only the employeeId, 
+    		// so we need to use the repository to get the full employee object
+    		currentEmployee = employeeRepository.findByEmployeeId(currentEmployee.getEmployeeId());
+    		List<Employee> directReports = currentEmployee.getDirectReports();
+    		if(directReports != null && directReports.size() > 0) {
+    			for(Employee report: directReports) {
+    				if(!distinctReports.contains(report)) {
+    					distinctReports.add(report);
+    					allReports.add(report);
+    				}
+    			}
+    		}
     	}
-	}
-	 
+    	return distinctReports.size();
+    }
 	
 	
 }
